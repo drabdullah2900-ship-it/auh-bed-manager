4261dc186a9f6be250a415c1148feec0516d68d9
drabdullah2900-ship-it <dr.abdullah2900@gmail.com>
2026-06-21 19:44:02 +0300

    Add wards data structure with room details

---
+++ wards.js
@@ -0,0 +1,86 @@
+const wards = [
+
+{
+id:"chest",
+name:"الصدرية والكلية",
+rooms:[
+{number:"401",beds:4},
+{number:"402",beds:2},
+{number:"403",beds:2},
+{number:"404",beds:2},
+{number:"405",beds:4},
+{number:"407",beds:4},
+{number:"408",beds:4},
+{number:"412",beds:1},
+{number:"418",beds:1}
+]
+},
+
+{
+id:"gastro",
+name:"الهضمية والعصبية",
+rooms:[
+{number:"غ1",beds:4},
+{number:"غ2",beds:4},
+{number:"غ3",beds:4},
+{number:"غ4",beds:4},
+{number:"غ7",beds:2},
+{number:"غ8",beds:2},
+{number:"غ9",beds:2},
+{number:"العناية العصبية",beds:4}
+]
+},
+
+{
+id:"icu",
+name:"العناية الداخلية",
+rooms:[
+{number:"1",beds:1},
+{number:"2",beds:1},
+{number:"3",beds:1},
+{number:"4",beds:1},
+{number:"5",beds:1},
+{number:"6",beds:1},
+{number:"7",beds:1},
+{number:"8",beds:1}
+]
+},
+
+{
+id:"endocrine",
+name:"العناية الغدية",
+rooms:[
+{number:"غدية",beds:2}
+]
+},
+
+{
+id:"third",
+name:"الثالث خاص",
+rooms:[
+{number:"غ2",beds:2},
+{number:"غ3",beds:2},
+{number:"غ4",beds:2},
+{number:"غ5",beds:2},
+{number:"غ6",beds:1}
+]
+},
+
+{
+id:"cardiac",
+name:"العناية القلبية",
+rooms:[
+{number:"غ2",beds:1},
+{number:"غ3",beds:1},
+{number:"غ4",beds:1},
+{number:"غ5",beds:1},
+{number:"غ6",beds:1},
+{number:"غ7",beds:1},
+{number:"غ8",beds:1},
+{number:"غ9",beds:1},
+{number:"غ10",beds:1}
+]
+}
+
+];
+export default wards;
